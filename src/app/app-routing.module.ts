89a968619f5bb794f98d114a3c88e6f4312d0f44
betomcp <beto.porto2000@gmail.com>
2022-08-18 15:21:29 -0300

    commit aul rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
+import { PageErrorComponent } from './pages/page-error/page-error.component';
+import { SobreComponent } from './pages/sobre/sobre.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component: HomeComponent, pathMatch: 'full' }, //quando tiver urls vazias precisa deste parâmetro (full)
+  { path: "sobre", component: SobreComponent },
+  { path: "404", component: PageErrorComponent },
+  { path: "**", redirectTo: '404' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
